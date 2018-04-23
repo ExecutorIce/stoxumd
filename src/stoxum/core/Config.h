7fb193b5c65e85887a1809bd42521db2613522ea
Mr. Stox <tool@stoxum.org>
2018-04-23 17:03:28 +0000

    Change default account resever for address

--- src/stoxum/core/Config.h
+++ src/stoxum/core/Config.h
@@ -154,8 +154,8 @@ public:
     boost::optional<std::size_t> VALIDATION_QUORUM;     // validations to consider ledger authoritative
 
     std::uint64_t                      FEE_DEFAULT = 10;
-    std::uint64_t                      FEE_ACCOUNT_RESERVE = SYSTEM_CURRENCY_PARTS / 1000;
-    std::uint64_t                      FEE_OWNER_RESERVE = SYSTEM_CURRENCY_PARTS / 4000;
+    std::uint64_t                      FEE_ACCOUNT_RESERVE = SYSTEM_CURRENCY_PARTS / 100;
+    std::uint64_t                      FEE_OWNER_RESERVE = SYSTEM_CURRENCY_PARTS / 400;
     std::uint64_t                      FEE_OFFER = 10;
 
     // Node storage configuration
